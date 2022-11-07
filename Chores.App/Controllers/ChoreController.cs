153dfd5bb72e707ed500890ca60719b0221a918a
Tyler Hagstrom <tyler.hagstrom@zelis.com>
2022-11-07 11:23:39 -0600

    Setup ability to create tests

---
+++ Chores.App/Controllers/ChoreController.cs
@@ -0,0 +1,45 @@
+﻿using Chores.Interfaces;
+using Chores.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chores.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ChoreController : ControllerBase
+{
+    private IDBInterface _choreDB;
+
+    private readonly ILogger<ChoreController> _logger;
+
+    public ChoreController(ILogger<ChoreController> logger, IDBInterface choreDB)
+    {
+        _logger = logger;
+        _choreDB = choreDB;
+    }
+
+    [HttpGet]
+    public List<Chore> Get()
+    {
+        return _choreDB.GetAllChores();
+    }
+
+    [HttpDelete]
+    public void Delete(int id)
+    {
+        _choreDB.DeleteChore(id);
+    }
+
+    [HttpPost]
+    public void Create(Chore newChore)
+    {
+        _choreDB.AddChore(newChore);
+    }
+
+    [HttpPut]
+    public void Update(Chore updatedChore)
+    {
+        _choreDB.EditChore(updatedChore);
+    }
+}
+
